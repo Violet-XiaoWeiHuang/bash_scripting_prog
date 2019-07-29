a47f846321f91185db80c45a71ca9cc418badb3c
root <root@DESKTOP-7UCNEHD.localdomain>
2019-07-28 22:17:07 -0400

    part 2: Calculate the number of directory levels to be displayed. This will be used to make sure we don't move down too far. Also, create another variable which keeps track of the current level for the additional information to be displayed.

--- pathdisplay.sh
+++ pathdisplay.sh
@@ -9,3 +9,20 @@ then
 	echo "$1 is not a valid directory name" >&2
 	exit 1
 fi 
+
+totalLevel=1
+currentLevel=1
+path=`cd $1 ; pwd`
+
+#echo $path
+
+cd $path                # go to the specified directory
+
+while [ `pwd` != "/" ]
+do
+    cd ..
+    (( totalLevel=totalLevel+1 ))
+done
+
+#echo $totalLevel
+currentLevel=$totalLevel
