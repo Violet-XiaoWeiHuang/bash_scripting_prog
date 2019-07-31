5e95e206ca8358966e06c9d9751319a53e9d44c1
root <root@DESKTOP-7UCNEHD.localdomain>
2019-07-31 15:17:10 -0400

    issue fixed: - If there is no argument specified, then the current directory will be the default. 	   - variable issue fixed

--- pathdisplay.sh
+++ pathdisplay.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ $# != 1 ]
+if [[ $# > 1 ]]
 then
 	echo "Usage: pathdisplay [ dir-name ]" >&2 
 	exit 1
-elif [ ! -d "$1" ]
+fi
+	
+if [ ! -d $1 ]
 then
 	echo "$1 is not a valid directory name" >&2
 	exit 1
@@ -26,12 +28,19 @@ done
 #echo $totalLevel
 currentLevel=$totalLevel
 
+
+key=
+while [ "$key" != "q" ]
+do
+
 clear
 tput cup 0 0
 
 echo "Owner   Group   Other   Filename"
 echo "-----   -----   -----   --------"
 echo ""
+
+key=
 
 for (( count = $totalLevel; count >= 1; count-- ))
 do 
@@ -47,9 +56,9 @@ do
 	dirFormat=$( echo $dirFormat | sed -r 's!^\/.*\/!!' | sed 's!/home!home!' )
 
 	echo "$perFormat $dirFormat"
+	#totalLevel$count=$count
 	echo ""
 
-#	ls -ld `pwd` | sed 's/  */ /g' | cut -d ' ' -f1,9 | sed 's/^d//' | sed -r 's/(.)(.)(.)(.)(.)(.)(.)(.)(.)/\1 \2 \3   \4 \5 \6   \7 \8 \9  /'
 	cd $path
 done
 
@@ -57,8 +66,24 @@ lines=`tput lines`
 tput cup $(( $lines-2 )) 0
 echo "Valid commands: u(p) d(own) q(uit)"
 
+
+# for-loop for extra infor required for the Current level
+for (( count = $currentLevel; count >= 1; count-- ))
+do
+					
+	for (( tmp = (( $totalLevel-$currentLevel )); tmp > 0; tmp-- ))
+	do
+		cd ..
+	done
+																	
+	tput cup $(( $currentLevel*2+2 )) 2
+	echo "currentLevel"
+
+done																
+
+
 tput cup $(( $totalLevel*2+1 )) 24
 echo -n ""
 read string
 
-
+done 	# end of while-loop
