35649008be27c2be42b53d040db38f2dbf48a948
root <root@DESKTOP-7UCNEHD.localdomain>
2019-08-01 10:39:35 -0400

    part d: Set up a loop that does the following until a "q" is entered. Don't worry yet about "enter" being required for user input: i. Inside the loop, display the screen of information using your code from the previous step. Keep track of the level being displayed so that you can use the current level variable to determine when to display the additional information. fixed issue: shown proper total levels

--- pathdisplay.sh
+++ pathdisplay.sh
@@ -1,89 +1,77 @@
 #!/bin/bash
 if [[ $# > 1 ]]
 then
-	echo "Usage: pathdisplay [ dir-name ]" >&2 
-	exit 1
+            echo "Usage: pathdisplay [ dir-name ]" >&2 
+            exit 1
 fi
-	
+            
 if [ ! -d $1 ]
 then
-	echo "$1 is not a valid directory name" >&2
-	exit 1
+            echo "$1 is not a valid directory name" >&2
+            exit 1
 fi
-
+ 
 totalLevel=1
-currentLevel=1
-path=`cd $1 ; pwd`
-
+cd $1
+path=$PWD
+ 
 #echo $path
-
-cd $path		# go to the specified directory 
-  	
-while [ `pwd` != "/" ]
+ 
+while [ $PWD != "/" ]
 do
-	cd ..
-	(( totalLevel=totalLevel+1 ))
+            totalLevel=$(( $totalLevel + 1 ))
+            cd ..
 done
-
-#echo $totalLevel
+ 
 currentLevel=$totalLevel
-
-
-key=
-while [ "$key" != "q" ]
-do
-
+ 
+displayPath(){
 clear
-tput cup 0 0
-
+ 
 echo "Owner   Group   Other   Filename"
 echo "-----   -----   -----   --------"
 echo ""
-
-key=
-
-for (( count = $totalLevel; count >= 1; count-- ))
-do 
-	for (( tmp = $count; tmp > 1; tmp-- ))
-	do
-		cd ..
-	done
-	
-	perFormat=$( ls -ld `pwd` | sed 's/  */ /g' | cut -d ' ' -f1 )
-	perFormat=$( echo $perFormat | sed 's/^d//' | sed 's/\.$//' | sed -r 's/(.)(.)(.)(.)(.)(.)(.)(.)(.)/\1 \2 \3   \4 \5 \6   \7 \8 \9  /' )
-
-	dirFormat=$( ls -ld `pwd` | sed 's/  */ /g' | cut -d ' ' -f9  )
-	dirFormat=$( echo $dirFormat | sed -r 's!^\/.*\/!!' | sed 's!/home!home!' )
-
-	echo "$perFormat $dirFormat"
-	#totalLevel$count=$count
-	echo ""
-
-	cd $path
+ 
+cd $path	# go to the specified directory 
+count=1		# compare to the currentLevel to deplay the extra infor or nor
+ 
+for dir in / $(pwd | tr '/' ' ')
+do
+            cd $dir             # get access to specified dir
+            
+            perFormat=$( ls -ld `pwd` | sed 's/  */ /g' | cut -d ' ' -f1 )
+            perFormat=$( echo $perFormat | sed 's/^d//' | sed 's/\.$//' | sed -r 's/(.)(.)(.)(.)(.)(.)(.)(.)(.)/\1 \2 \3   \4 \5 \6   \7 \8 \9   /' )
+            
+            echo "${perFormat}$dir"
+            
+            if [ $count = $currentLevel ]
+            then
+                        ls -ld | awk ' {print "   Links: " $2 " Owner: " $3 " Group: " $4 " Size: " $5 " Modified: " $6, $7, $8}'
+            fi
+                        echo ""
+            (( count++ ))
 done
-
+ 
+echo "totalLevel: $totalLevel"
+ 
 lines=`tput lines`
 tput cup $(( $lines-2 )) 0
 echo "Valid commands: u(p) d(own) q(uit)"
-
-
-# for-loop for extra infor required for the Current level
-for (( count = $currentLevel; count >= 1; count-- ))
-do
-					
-	for (( tmp = (( $totalLevel-$currentLevel )); tmp > 0; tmp-- ))
-	do
-		cd ..
-	done
-																	
-	tput cup $(( $currentLevel*2+2 )) 2
-	echo "currentLevel"
-
-done																
-
-
-tput cup $(( $totalLevel*2+1 )) 24
-echo -n ""
+ 
+tput cup $(( $currentLevel*2+1 )) 24
+}           # end of displayPath()             
+ 
+ 
+#key=
+#while [ "$key" != "q" ]
+#do
+ 
+displayPath
 read string
-
-done 	# end of while-loop
+ 
+ 
+# tput cup $(( $totalLevel*2+1 )) 24
+# echo -n ""
+# read string
+ 
+#done # end of while-loop
